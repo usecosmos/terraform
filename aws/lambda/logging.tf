f0b0cc614c8a36362c96baf966d3385891505ddb
marfoldi <peter@dbl.works>
2024-10-19 17:46:39 +0200

    feat: expose cloudwatch_logs_retention_in_days

--- aws/lambda/logging.tf
+++ aws/lambda/logging.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${var.function_name}"
-  retention_in_days = 90
+  retention_in_days = var.cloudwatch_logs_retention_in_days
 
   tags = {
     Project     = var.project
